58ae72d30c997d2923131221d1ace3ac8129b80c
=Jubayer <=Jubayer356@gmail.com>
2021-06-24 04:34:41 +0600

    Udemy poor copy coming soon

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Header</h1>
+        </div>
+    );
+};
+
+export default Header;
